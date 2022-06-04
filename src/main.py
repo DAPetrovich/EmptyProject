a448f8098227b38b26749e3888f249d1016999e2
Anatoliy <a.dorosh@sberautopark.onmicrosoft.com>
2022-06-04 16:11:02 +0300

    delete import from main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from src.api.routes import api_router
-from src.settings.database import db, engine, metadata
+from src.settings.database import db
 
 app = FastAPI()
 
